d4d68cd0b07ad63e5d6ecad0a005c0691dcbe543
padjal <padjal136@gmail.com>
2023-06-12 00:57:34 +0300

    Add bookservice
    Added bookService and implemented find by author.

---
+++ src/OpenLibrary/OpenLibrary.Core/Services/BookService.cs
@@ -0,0 +1,64 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using OpenLibrary.Core.Interfaces;
+using OpenLibrary.Core.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Json;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace OpenLibrary.Core.Services
+{
+    public class BookService : IBookService
+    {
+        private readonly HttpClient _httpClient;
+
+        public BookService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<List<Book>> GetBooksByAuthorAsync(string author)
+        {
+            var books = new List<Book>();
+
+             var response = await _httpClient.GetAsync($"?author={author}");
+
+            if(response.IsSuccessStatusCode)
+            {
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                var data = JObject.Parse(jsonString);
+
+                var booksCollection = data["docs"].Children().ToList();
+
+                foreach (var book in booksCollection)
+                {
+                    var bookTitle = book["title"].Value<string>();
+                    var bookAuthor = book["author_name"].Children().FirstOrDefault().Value<string>();
+
+                    books.Add(new Book
+                    {
+                        Title = bookTitle,
+                        Author = bookAuthor
+                    });
+                }
+            }
+            else
+            {
+                throw new NotImplementedException();
+
+            }
+
+            return books;
+        }
+
+        public Task<List<Book>> GetBooksByTitleAsync(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- src/OpenLibrary/OpenLibrary.Presentation/ViewModels/SearchViewModel.cs
+++ src/OpenLibrary/OpenLibrary.Presentation/ViewModels/SearchViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Input;
+using OpenLibrary.Core.Interfaces;
 using OpenLibrary.Core.Models;
 using OpenLibrary.Presentation.Core;
 using OpenLibrary.Presentation.Interfaces;
@@ -13,19 +16,26 @@ namespace OpenLibrary.Presentation.ViewModels
 {
     public class SearchViewModel : ViewModelBase
     {
+        private readonly IBookService _bookService;
+        private readonly BookInfoViewModel _bookInfoViewModel;
         private INavigationService _navigationService;
         private Book _selectedBook;
         private string _searchAuthor;
         private string _searchTitle;
+        private ObservableCollection<Book> _books;
+        private string _resultLabel;
 
-        public SearchViewModel(INavigationService navigationService)
+        public SearchViewModel(INavigationService navigationService, IBookService bookService, BookInfoViewModel bookInfoViewModel)
         {
+            _bookInfoViewModel = bookInfoViewModel;
             NavigationService = navigationService;
-
+            _bookService = bookService;
             NavigateBookInfoCommand = new RelayCommand(o => NavigationService.NavigateTo<BookInfoViewModel>(), o => true);
 
             AfterSearchTitleUpdated = new RelayCommand(o => { SearchAuthor = string.Empty; }, o => true);
             AfterSearchAuthorUpdated = new RelayCommand(o => { SearchTitle = string.Empty; }, o => true);
+
+            Books = new ObservableCollection<Book>();
         }
 
         public INavigationService NavigationService
@@ -44,16 +54,24 @@ namespace OpenLibrary.Presentation.ViewModels
 
         public ICommand AfterSearchAuthorUpdated { get; set; }
 
-        public ObservableCollection<Book> Books { get; set; } = new ObservableCollection<Book> 
+        public ICommand SearchCommand => new RelayCommand(o => Search(), o => true);
+
+        public ObservableCollection<Book> Books 
+        {
+            get => _books;
+            set => SetField(ref _books, value);
+        }
+
+        /*= new ObservableCollection<Book>
         {
             //TODO: Remove. Used only for development
             new Book
             {
-                Title = "Oliver Twist", 
+                Title = "Oliver Twist",
                 Author = "Charles Dikens",
                 PageCount = 678,
                 Isbn = "SDFKSDJFO90808DFSF",
-                PublishedTime = DateTime.Now,
+                PublishedYear = 2005,
             },
             new Book
             {
@@ -61,15 +79,19 @@ namespace OpenLibrary.Presentation.ViewModels
                 Author = "Charles Dikens",
                 PageCount = 678,
                 Isbn = "SDFKSDJFO90808DFSF",
-                PublishedTime = DateTime.Now,
+                PublishedYear = 2004,
             }
-        };
+        };*/
 
         public Book SelectedBook
         {
             get => _selectedBook;
             set
             {
+                SetField(ref _selectedBook, value);
+
+                _bookInfoViewModel.Book = SelectedBook;
+
                 NavigationService.NavigateTo<BookInfoViewModel>();
             }
         }
@@ -84,6 +106,45 @@ namespace OpenLibrary.Presentation.ViewModels
         {
             get => _searchTitle;
             set => SetField(ref _searchTitle, value);
-        }   
+        }
+
+        public string ResultLabel
+        {
+            get => _resultLabel;
+            set => SetField(ref _resultLabel, value);
+        }
+
+        private async Task Search()
+        {
+            if (!string.IsNullOrEmpty(SearchAuthor))
+            {
+                //Search by author
+
+                var timer = new Stopwatch();
+                timer.Start();
+
+                //Show busy indicator
+                var result = await _bookService.GetBooksByAuthorAsync(SearchAuthor);
+
+                Books.Clear();
+
+                Books = new ObservableCollection<Book>(result);
+
+                timer.Stop();
+                ResultLabel = $"{Books.Count} results found in {timer.Elapsed.TotalMilliseconds} ms";
+                //Disable busyIndicator
+
+            }
+            else if (!string.IsNullOrEmpty(SearchTitle))
+            {
+                //Search by title
+            }
+            else
+            {
+                MessageBox.Show("No search criteria found.\n" +
+                    "Please add either a book title or author and try again.",
+                    "Search error");
+            }
+        }
     }
 }
